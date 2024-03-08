32fe76ffe3fc3875651cd25a9f5747f82a7f1de3
Comrade xploit <79577718+comradex56@users.noreply.github.com>
2024-03-08 18:26:30 +0530

    userbot.py

--- AnonXMusic/core/userbot.py
+++ AnonXMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("O_P_Hacker")
-                await self.one.join_chat("Hacker_fed")
+                await self.one.join_chat("https://t.me/ananyamusicBot6789")
+                await self.one.join_chat("https://t.me/ananyamusicBot6789")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("O_P_Hacker")
-                await self.one.join_chat("Hacker_fed")
+                await self.two.join_chat("https://t.me/ananyamusicBot6789")
+                await self.one.join_chat("https://t.me/ananyamusicBot6789")
             except:
                 pass
             assistants.append(2)
@@ -93,8 +93,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("O_P_Hacker")
-                await self.one.join_chat("Hacker_fed")
+                await self.three.join_chat("https://t.me/ananyamusicBot6789")
+                await self.one.join_chat("https://t.me/ananyamusicBot6789")
             except:
                 pass
             assistants.append(3)
@@ -114,8 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("O_P_Hacker")
-                await self.one.join_chat("Hacker_fed")
+                await self.four.join_chat("https://t.me/ananyamusicBot6789")
+                await self.one.join_chat("https://t.me/ananyamusicBot6789")
             except:
                 pass
             assistants.append(4)
